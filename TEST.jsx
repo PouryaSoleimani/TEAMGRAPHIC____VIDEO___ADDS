b0fe5cce3e50e732e5b81bd4b83d40ed14bf0710
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-03-10 13:41:11 +0330

    🟩🟩🟩🟩 RESPONSIVE 🟩🟩🟩🟩

---
+++ TEST.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+
+const TEST = () => {
+   const [number, setNumber] = useState(0)
+   function NumberAddHandler() {
+      if (number < 100) {
+         setNumber(prev => prev + 1)
+      }
+   }
+   return (
+      <div>TEST</div>
+   )
+}
+
+export default TEST
