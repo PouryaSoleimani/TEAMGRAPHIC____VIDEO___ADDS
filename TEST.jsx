09d48d4b47af9281db80d40789d9e9ecbf43e67e
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-03-10 14:14:37 +0330

    🟩🟩🟩🟩 RESPONSIVE 🟩🟩🟩🟩

--- TEST.jsx
+++ TEST.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react'
 
 const TEST = () => {
+
    const [number, setNumber] = useState(0)
+
    function NumberAddHandler() {
-      if (number < 100) {
-         setNumber(prev => prev + 1)
-      }
+      if (number < 100) { setNumber(prev => prev + 1) }
    }
    return (
       <div>TEST</div>
